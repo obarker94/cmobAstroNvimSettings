4b989c567c124f061369781bbade36ff4b10d100
obarker94 <me@oliverbarker.io>
2023-03-12 04:04:17 +0000

    updated plugins and settings with copilot and buffers left/right

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,7 +1,8 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
+  "andweeb/presence.nvim",
+  "zbirenbaum/copilot.lua",
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
